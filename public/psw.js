9065ada97476a383397690b2bfaa02b887cdc0ac
Gabe <66077254+MrBrain295@users.noreply.github.com>
2023-11-18 08:42:22 -0600

    var -> const/let

--- public/psw.js
+++ public/psw.js
@@ -1,21 +1,19 @@
-
-
 importScripts('/dip/dip.worker.js');
 importScripts('/uv/uv.sw.js');
 
-var DIP = new DIPServiceWorker('/dip/dip.config.js');
-var UV = new UVServiceWorker();
+const DIP = new DIPServiceWorker('/dip/dip.config.js');
+const UV = new UVServiceWorker();
 
 self.addEventListener('install', event => {
   self.skipWaiting();
-})
+});
 
 self.addEventListener('activate', event => {
   event.waitUntil(clients.claim());
 });
 
 async function fetchHandler(event) {
-  var request = event.request;
+  const request = event.request;
   
   if (event.request.url.startsWith(location.origin+'/~/dip')) return await DIP.fetch(event);
   if (event.request.url.startsWith(location.origin+'/~/uv')) return await UV.fetch(event);
@@ -26,5 +24,5 @@ async function fetchHandler(event) {
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     fetchHandler(event)
-  )
-})
+  );
+}
